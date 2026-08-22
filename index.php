f884ddd2659a6b322c0238be0dae5d7ea9846a2c
Rohit Sengar <138657811+rohitsengar-aws@users.noreply.github.com>
2026-08-22 15:00:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -570,7 +570,9 @@
         <div class="row align-items-center g-5">
           <div class="col-lg-6">
             <div class="volunteer-visual">
-              <div class="volunteer-circle"><i class="fa-solid fa-people-group"></i></div>
+              <div class="volunteer-circle">
+                <img src="/images/Volunteers-are-the-heart-of-VBCCS.png" alt="" class="w-100">
+              </div>
               <div class="mini-card mc-one"><i class="fa-solid fa-hand-holding-heart"></i><span>Serve</span></div>
               <div class="mini-card mc-two"><i class="fa-solid fa-people-arrows"></i><span>Connect</span></div>
               <div class="mini-card mc-three"><i class="fa-solid fa-seedling"></i><span>Grow</span></div>
